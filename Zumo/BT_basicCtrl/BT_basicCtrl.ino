d3bfd7997ea290f59744b658381204457efa8edc
Creasote <chris@mrcreasote.com>
2014-08-23 14:53:30 +1000

    Buzzer and LED
    Added buzzer and LED code to existing BT_basicCtrl

--- Zumo/BT_basicCtrl/BT_basicCtrl.ino
+++ Zumo/BT_basicCtrl/BT_basicCtrl.ino
@@ -2,11 +2,69 @@
 #include <ZumoReflectanceSensorArray.h>
 #include <ZumoMotors.h>
 #include <ZumoBuzzer.h>
-#define LED_PIN       13 // user LED pin
+#include <Adafruit_NeoPixel.h>
+#include <avr/pgmspace.h> // Required for flash storage
+
+//=================
+//       AUX
+//=================
+//**** No Aux ****
+#define NO_AUX 1
+
+//**** Colours ****
+#define BLACK 2
+#define RED 3
+#define YELLOW 4
+#define GREEN 5
+#define CYAN 6
+#define BLUE 7
+#define MAGENTA 8
+#define WHITE 9
+
+//**** Buzzer ****
+#define POWERUP 10
+#define POWERDOWN  11
+
+//**** sounds ****
+const char getPowerUp[] PROGMEM =
+// Powerup sound from Mario
+"T500 L8 V15 ML gb>d>g>b a->c>e->a->>c b->d>f>b->>d";
+
+const char usePowerUp[] PROGMEM =
+// The classic star theme
+"T300 L4 V15 MS >c>c>c ML d8>c8 R8 MS >c ML d8>c8d8 MS >c bbb ML c8b8 r8 MS b ML c8b8c8 MS b"
+"MS >c>c>c ML d8>c8 R8 MS >c ML d8>c8d8 MS >c bbb ML c8b8 r8 MS b ML c8b8c8 MS b"
+"MS >c>c>c ML d8>c8 R8 MS >c ML d8>c8d8 MS >c bbb ML c8b8 r8 MS b ML c8b8c8 MS b"
+"MS >c>c>c ML d8>c8 R8 MS >c ML d8>c8d8 MS >c";
+
+const char hazard[] PROGMEM = 
+// Just the powerup theme, lowered an octave and in reverse
+"T400 O3 L8 V15 ML >>d>b->f>db- >>c>a->e->ca- >b>g>dbg";
+
+const char coin[] PROGMEM = 
+// Optional sound effect; the "coin" sound from Mario 64
+"T400 L8 V15 ML >b8 >>e2..";
+//-------------------------------------------------------------------------------
+
+uint32_t colour[] = {0x00000000,  //BLACK
+                     0x0000FF00,  //RED
+                     0x00FFFF00,  //YELLOW
+                     0x00FF0000,  //GREEN
+                     0x00FF00FF,  //CYAN
+                     0x000000FF,  //BLUE
+                     0x00FF00FF,  //MAGENTA
+                     0x00FFFFFF}; //WHITE
+                     
+#define stripLen 2
+#define stripPin 2
 
 ZumoBuzzer buzzer;
+Adafruit_NeoPixel ledStrip(stripLen, stripPin, NEO_RGB + NEO_KHZ800);
+
+#define LED_PIN       13 // user LED pin
+
 ZumoReflectanceSensorArray reflectanceSensors;
-int left_speed, right_speed, silenceCount;
+int left_speed, right_speed, silenceCount, AUX;
 unsigned int sensors[6];  // values from sensor array
 int speed_reduction;      // speed reduction factor 
 float minSpeed = 0.2;     // minimum speed after scaling
@@ -16,6 +74,7 @@ void setup()   {
  //initialize the pins as digital outputs
   buzzer.play(">g32>>c32");
   pinMode(LED_PIN, OUTPUT); 
+  ledStrip.begin(); 
   Serial.begin(9600); // opens a serial port
   
   // Initialize the reflectance sensors module
@@ -64,9 +123,8 @@ void loop() {
     // 2nd byte: rspeed 0=full rev, 64=halt, 127=full fwd
     left_speed = (buf[0]-64) * 25 / 4; // 400:64 maxSpeed:0.5byteRange
     right_speed = (buf[1]-64) * 25 / 4;
-<<<<<<< HEAD
-   
-=======
+    AUX = buf[2];
+    
     // Apply track detection / speed reduction
     reflectanceSensors.readCalibrated(sensors, QTR_EMITTERS_ON);
     
@@ -79,7 +137,6 @@ void loop() {
 
     left_speed *= scaleFactor;
     right_speed *= scaleFactor;
->>>>>>> origin/master
   } else {
     silenceCount++;
     if (silenceCount>10000) {
@@ -88,4 +145,30 @@ void loop() {
     }
   }
   ZumoMotors::setSpeeds(left_speed, right_speed);
+  
+  if(AUX != NO_AUX)
+  {
+    if(AUX >= BLACK && AUX <= WHITE)
+    {
+      setRGB(AUX);
+    }
+    else if(AUX == POWERUP)
+    {
+      buzzer.playFromProgramSpace(getPowerUp);
+    }
+    else if(AUX == POWERDOWN)
+    {
+      buzzer.playFromProgramSpace(hazard);
+    }
+  }
+  
+}
+
+// Using Neo_Pixel, set both rear LED to colour given by AUX byte
+void setRGB(int col)
+{
+  for(int led = 0; led < stripLen; led++){
+    ledStrip.setPixelColor(led,colour[col-2]);
+  }
+  ledStrip.show();
 }
